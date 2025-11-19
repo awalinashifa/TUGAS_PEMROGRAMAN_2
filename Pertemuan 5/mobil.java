0ea7b0c69c8b422ea2ef26f111b69584b0ce5846
cipz <awalinasyifafy@gmail.com>
2025-11-19 17:54:15 +0700

    Add files via upload

---
+++ Pertemuan 5/mobil.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan_5;
+
+/**
+ *
+ * @author macbook
+ */
+public class mobil {
+    public void jalankan (){
+        System.out.println("Masukkan gigi 1, lepas kopling dan tekan gas...");
+    }
+}

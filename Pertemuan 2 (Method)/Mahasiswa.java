bc4fb4c335ee8a87c71adfd7fadcd46e227be257
cipz <awalinasyifafy@gmail.com>
2025-11-29 11:41:37 +0700

    Add files via upload

---
+++ Pertemuan 2 (Method)/Mahasiswa.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan_2_method;
+
+/**
+ *
+ * @author pc14
+ */
+
+class Mahasiswa {
+    String nama;
+    String nim;
+
+    void tampilkanData() {
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM  : " + nim);
+    }
+}
+
+// Class turunan
+class MahasiswaAktif extends Mahasiswa {
+    String jurusan;
+
+    void tampilkanJurusan() {
+        System.out.println("Jurusan : " + jurusan);
+    }
+}

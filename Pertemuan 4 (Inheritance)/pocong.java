e57487c2b24c8873b0fe70ce9bab4d6f41a95b42
cipz <awalinasyifafy@gmail.com>
2025-11-29 11:52:38 +0700

    Add files via upload

---
+++ Pertemuan 4 (Inheritance)/pocong.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan_4_inheritance;
+
+/**
+ *
+ * @author cipa
+ */
+public class pocong extends enemy {
+    void jump(){
+        System.out.println("Cong Jump.");
+    }
+            
+}

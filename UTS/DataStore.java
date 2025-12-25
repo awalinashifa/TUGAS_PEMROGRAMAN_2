3dab8f19ff0887dc172be5dd70875db987f05dd2
cipz <awalinasyifafy@gmail.com>
2025-12-26 00:17:26 +0700

    Add files via upload

---
+++ UTS/DataStore.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts;
+
+/**
+ *
+ * @author macbook
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataStore {
+    public static List<Object[]> dataAbsensi = new ArrayList<>();
+}
